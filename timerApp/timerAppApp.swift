f3f3c966b8dd4be84413157950ba3a1b60688e3c
Miyuki <87884079+miyuki-KK@users.noreply.github.com>
2023-10-30 13:44:35 +0900

    Initial Commit

---
+++ timerApp/timerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  timerAppApp.swift
+//  timerApp
+//
+//  Created by Miyuki Kikuchi on 30/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct timerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
